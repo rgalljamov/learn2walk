a9292213ce8d9c742eb4808444365e79e4caef84
Rustam Galljamov <rustam.galljamov@gmail.com>
2020-09-10 21:05:48 +0200

    ENV: Implement a torque controller 3D walker

--- mujoco/gym_mimic_envs/mujoco/mimic_walker3d.py
+++ mujoco/gym_mimic_envs/mujoco/mimic_walker3d.py
@@ -33,8 +33,10 @@ class MimicWalker3dEnv(MimicEnv, mujoco_env.MujocoEnv, utils.EzPickle):
     '''
 
     def __init__(self):
+        walker_xml = {'mim3d': 'walker3pd.xml',
+                      'mim_trq3d': 'walker3d.xml'}[cfg.env_name]
         mujoco_env.MujocoEnv.__init__(self,
-                                      join(dirname(__file__), "assets", "walker3pd.xml"), 4)
+                                      join(dirname(__file__), "assets", walker_xml), 4)
         utils.EzPickle.__init__(self)
         # init the mimic environment, automatically loads and inits ref trajectories
         global qpos_indices, qvel_indices
